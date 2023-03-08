a19735f932b5b4b789fffd2ae45a8de3da80ea7e
kerry <kmenglin@usc.edu>
2023-03-07 19:21:02 -0800

    basic log-in page, only needs APIURL

--- site/src/App.js
+++ site/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Other from "./pages/Other";
+import LogIn from "./pages/LogIn";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         {/* Root pages, located in /pages/ */}
         <Route path="/" element={<Home />} />
         <Route path="/other" element={<Other />} />
+        <Route path="/LogIn" element={<LogIn />} />
         {/* 404 page not found redirect */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
---
+++ site/src/pages/LogIn.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+function LogIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function handleLoginClick() {
+    // Construct the API request
+    const apiUrl = 'https://example.com/login';
+    const requestData = {
+      email: email,
+      password: password
+    };
+    const requestHeaders = {
+      'Content-Type': 'application/json'
+    };
+    const requestOptions = {
+      method: 'POST',
+      headers: requestHeaders,
+      body: JSON.stringify(requestData)
+    };
+
+    // Send the API request
+    fetch(apiUrl, requestOptions)
+      .then(response => {
+        // Handle the response
+        if (response.ok) {
+          // Redirect the user to the dashboard or home page
+          window.location.href = 'https://example.com/dashboard';
+        } else {
+          // Display an error message to the user
+          setErrorMessage('Invalid email or password');
+        }
+      })
+      .catch(error => {
+        // Handle the error
+        console.error(error);
+        setErrorMessage('An error occurred, please try again later');
+      });
+  }
+
+  return (
+    <div>
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
+      <button onClick={handleLoginClick}>Login</button>
+      {errorMessage && <div>{errorMessage}</div>}
+    </div>
+  );
+}
+
+export default LogIn;
