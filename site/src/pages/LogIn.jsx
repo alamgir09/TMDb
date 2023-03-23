928bcda043df53f7ef266991a121e9234af1ad0d
kerry <kmenglin@usc.edu>
2023-03-23 11:40:40 -0700

    removed useNavigate from LogIn & SignUp

--- site/src/pages/LogIn.jsx
+++ site/src/pages/LogIn.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 function LogIn() {
   // fetchResponse is a constant in this component's state. Use handleFetchResponse(newValue)
