2afa28b628f92c6b4dd5460b3d9fc0fe38163e6c
Kevin Dong <kevindon@usc.edu>
2023-04-01 00:08:00 -0700

    fix jest to pass all tests

--- site/src/_tests_/MovieBox.test.js
+++ site/src/_tests_/MovieBox.test.js
@@ -1,96 +1,42 @@
-// import React from "react";
-// import { render, screen, waitFor } from "@testing-library/react";
-// import App from "../App";
-// import { BrowserRouter } from "react-router-dom";
-// import userEvent from "@testing-library/user-event";
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { useNavigate } from "react-router-dom";
+import MovieBox from "../components/MovieBox";
 
-// beforeEach(() => {
-//   fetch.resetMocks();
-// });
+jest.mock("react-router-dom", () => ({
+  useNavigate: jest.fn()
+}));
 
-// // Reset the browser history after each test
-// afterEach(() => {
-//   window.history.pushState(null, document.title, "/Search");
-// });
+describe("MovieBox", () => {
+  const props = {
+    id: "1",
+    imgURL: "https://example.com/movie.jpg",
+    title: "Example Movie",
+    release_date: "2022-01-01",
+    rating: "8.5"
+  };
 
-// test("that math works", async () => {
-//   expect(5 + 5).toBe(10);
-// });
+  it("renders a movie button", () => {
+    const { getByText } = render(<MovieBox {...props} />);
+    expect(getByText(props.title)).toBeInTheDocument();
+  });
 
-// test("does movie box accurately set up boxes", async () => {
-//   const user = userEvent.setup();
-//   render(<App />, { wrapper: BrowserRouter });
+  it("renders a movie button with null values", () => {
+      const { getAllByText } = render(<MovieBox id={""} imgURL={""} title={""} release_date={""} rating={""} />);
 
-//   // verify page content for default route
-//   expect(screen.getByText(/Add Watch List/)).toBeInTheDocument();
+      expect(getAllByText("?")[0]).toBeInTheDocument();
+    });
 
-//   // verify page content for expected route after navigating
-//   await waitFor(() => user.click(screen.getByText(/click to go to other page/i)));
-//   expect(screen.getByText(/Other Page/)).toBeInTheDocument();
+  it("renders the movie image with the expected URL and alt text", () => {
+    const { getByAltText } = render(<MovieBox {...props} />);
+    expect(getByAltText(props.title)).toHaveAttribute("src", props.imgURL);
+  });
 
-//   await waitFor(() => user.click(screen.getByText(/click to go to home page/i)));
-//   expect(screen.getByText(/Home Page/)).toBeInTheDocument();
-// });
-
-// test("fetching works on the home page", async () => {
-//   fetch.mockResponseOnce(JSON.stringify({ data: "Pong Frontend. Received at 2023-02-25T20:49:00.813447Z." }));
-
-//   const user = userEvent.setup();
-//   render(<App />, { wrapper: BrowserRouter });
-
-//   // verify page content for default route
-//   expect(screen.getByText(/Home Page/)).toBeInTheDocument();
-//   await waitFor(() => user.click(screen.getByText(/fetch backend/i)));
-//   expect(screen.getByText(/pong frontend./i)).toBeInTheDocument();
-
-//   expect(fetch).toHaveBeenCalledTimes(1);
-// });
-
-// test("fetching fails on the home page with no connection", async () => {
-//   fetch.mockRejectOnce(new Error("API is down"));
-
-//   const user = userEvent.setup();
-//   render(<App />, { wrapper: BrowserRouter });
-
-//   expect(screen.getByText(/Home Page/)).toBeInTheDocument();
-//   await waitFor(() => user.click(screen.getByText(/fetch backend/i)));
-//   expect(screen.getByText(/An API error occured/i)).toBeInTheDocument();
-
-//   expect(fetch).toHaveBeenCalledTimes(1);
-// });
-
-// test("fetching fails on the home page with malformed API response", async () => {
-//   fetch.mockResponseOnce(JSON.stringify({ data: null }));
-
-//   const user = userEvent.setup();
-//   render(<App />, { wrapper: BrowserRouter });
-
-//   expect(screen.getByText(/Home Page/)).toBeInTheDocument();
-//   await waitFor(() => user.click(screen.getByText(/fetch backend/i)));
-
-//   expect(fetch).toHaveBeenCalledTimes(1);
-// });
-
-// test("counting works on the other page", async () => {
-//   const user = userEvent.setup();
-//   render(<App />, { wrapper: BrowserRouter });
-
-//   // verify page content for default route
-//   expect(screen.getByText(/Home Page/)).toBeInTheDocument();
-
-//   // verify page content for expected route after navigating
-//   await waitFor(() => user.click(screen.getByText(/click to go to other page/i)));
-//   expect(screen.getByText(/current state counter/i)).toBeInTheDocument();
-
-//   await waitFor(() => user.click(screen.getByText(/increment counter/i)));
-//   await waitFor(() => user.click(screen.getByText(/increment counter/i)));
-//   expect(screen.getByText(/current state counter: 2/i)).toBeInTheDocument();
-//   await waitFor(() => user.click(screen.getByText(/clear counter/i)));
-//   expect(screen.getByText(/current state counter: 0/i)).toBeInTheDocument();
-//   let counter = 12;
-//   for (let i = 0; i < counter; i++) {
-//     await waitFor(() => user.click(screen.getByText(/increment counter/i)));
-//   }
-//   expect(screen.getByText(/current state counter: 12/i)).toBeInTheDocument();
-//   expect(screen.getByText(/Counter is greater than 10./i)).toBeInTheDocument();
-// });
+  it("calls navigate when the button is clicked", () => {
+    const navigate = jest.fn();
+    useNavigate.mockReturnValue(navigate);
+    const { getByText } = render(<MovieBox {...props} />);
+    fireEvent.click(getByText(props.title));
+    expect(navigate).toHaveBeenCalledWith(`/movies/${props.id}`);
+  });
+});
