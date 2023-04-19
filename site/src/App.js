404c91592481568ffdd9f94219bdef29bc998f72
Jasmine Lee <jlee6488@usc.edu>
2023-04-18 18:24:19 -0700

    changed default to home page

--- site/src/App.js
+++ site/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-//import Home from "./pages/Home";
+import Home from "./pages/Home";
 import Other from "./pages/Other";
 import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
@@ -16,7 +16,7 @@ function App() {
     <div>
       <Routes>
         {/* Root pages, located in /pages/ */}
-        <Route path="/" element={<LogIn />} />
+        <Route path="/" element={<Home />} />
         <Route path="/other" element={<Other />} />
         <Route path="/LogIn" element={<LogIn />} />
         <Route path="/SignUp" element={<SignUp />} />
